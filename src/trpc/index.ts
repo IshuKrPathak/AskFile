4ee21a0fd832b9a16dd58dfc9080fe3cec94e3e9
Ishu Kumar Pathak <pishu1766@gmail.com>
2024-02-14 13:57:15 +0530

    neon database connected

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,12 +1,28 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 export const appRouter = router({
-  authCallBack: publicProcedure.query(() => {
+  authCallBack: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
     // checks if user is already present in database
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+    if (!dbUser) {
+      //create new iuser
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
     return { success: true };
   }),
 });
