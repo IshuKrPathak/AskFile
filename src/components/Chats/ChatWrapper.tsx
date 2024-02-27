21f2c4a98f0c29aae623457e7bf1b0b3cb5e38da
Ishu Kumar Pathak <pishu1766@gmail.com>
2024-02-27 13:01:40 +0530

    chat input section

--- src/components/Chats/ChatWrapper.tsx
+++ src/components/Chats/ChatWrapper.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import Messages from "./Messages";
 import ChatInput from "./ChatInput";
 import { trpc } from "@/app/_trpc/client";
-import { Loader2 } from "lucide-react";
+import { ChevronLeft, Loader2, XCircleIcon } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
 
 interface ChatWrapperProps {
   fileId: string;
@@ -21,7 +23,9 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
     }
   );
 
-  if (true)
+  //loading state
+
+  if (isLoading)
     return (
       <div className=" relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
         <div className=" flex-1 flex justify-center items-center flex-col mb-28">
@@ -31,6 +35,63 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
             <p className=" text-zinc-500 text-sm">
               We&apos;re preparing your PDF. This might take a few seconds.
             </p>
+          </div>
+        </div>
+        <ChatInput isDisabled />
+      </div>
+    );
+
+  //processing state
+
+  if (data?.status === "PROCESSING")
+    return (
+      <div className=" relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className=" flex-1 flex justify-center items-center flex-col mb-28">
+          <div className=" flex flex-col items-center gap-2">
+            <Loader2 className=" h-8 w-8 text-orange-500 animate-spin" />
+            <h3 className=" font-semibold text-xl">Processing..</h3>
+            <p className=" text-zinc-500 text-sm">
+              This won&apos;t take long period. You can continue to chat in the
+              meantime.
+            </p>
+          </div>
+        </div>
+        <ChatInput isDisabled />
+      </div>
+    );
+
+  // failed state
+
+  if (data?.status === "FAILED")
+    return (
+      <div className=" relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className=" flex-1 flex justify-center items-center flex-col mb-28">
+          <div className=" flex flex-col items-center gap-2">
+            <XCircleIcon className=" h-8 w-8 text-red-500 " />
+            <h3 className=" font-semibold text-xl">Too many pages in PDF</h3>
+            <p className=" text-zinc-500 text-sm">
+              Your <span className=" font-medium ">Free</span> plan supports
+              upto 5 pages per PDF.
+              <br />
+              Please upgrade to
+              <a
+                href="/pricing"
+                className="text-yellow-500 underline items-center relative"
+              >
+                🌟Premium{" "}
+              </a>
+              plan for extensive use.
+            </p>
+            <Link
+              href="/dashboard"
+              className={buttonVariants({
+                variant: "secondary",
+                className : "mt-4",
+              })}
+            >
+              <ChevronLeft className=" h-3 w-3 mr-1.5 " />
+              Back
+            </Link>
           </div>
         </div>
         <ChatInput isDisabled />
