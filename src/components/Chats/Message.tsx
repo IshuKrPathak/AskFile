24221cc075434ff390b0c19a8a62370bcf763b23
Ishu Kumar Pathak <pishu1766@gmail.com>
2024-03-01 15:07:59 +0530

    dispalying messages completed

--- src/components/Chats/Message.tsx
+++ src/components/Chats/Message.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/lib/utils";
 import { ExtendedtMessage } from "@/types/message";
 import { Icons } from "../icons";
+import ReactMarkdown from "react-markdown";
+import { format } from "date-fns";
 
 interface MessageProps {
   message: ExtendedtMessage;
@@ -31,6 +33,46 @@ const Message = ({ message, isNextMessageSamePerson }: MessageProps) => {
         ) : (
           <Icons.logo className=" fill-zinc-300 h-3/4 w-3/4" />
         )}
+      </div>
+
+      <div
+        className={cn("flex flex-col  space-y-2 text-base max-w-md mx-2  ", {
+          "order-1 items-end ": message.isUserMessage,
+          "order-2  items-start": !message.isUserMessage,
+        })}
+      >
+        <div
+          className={cn("px-4 py-2 rounded-lg inline-block", {
+            "bg-orange-500 text-white": message.isUserMessage,
+            "bg-gray-200  text-gray-900": !message.isUserMessage,
+            "rounded-br-none":
+              !isNextMessageSamePerson && message.isUserMessage,
+            "rounded-bl-none":
+              !isNextMessageSamePerson && !message.isUserMessage,
+          })}
+        >
+          {typeof message.text === "string" ? (
+            <ReactMarkdown
+              className={cn("prose", {
+                "text-zinc-50": message.isUserMessage,
+              })}
+            >
+              {message.text}
+            </ReactMarkdown>
+          ) : (
+            message.text
+          )}
+          {message.id !== "loading-message" ? (
+            <div
+              className={cn("text-xs select-none mt-2 w-full text-right ", {
+                "text-zinc-500  ": !message.isUserMessage,
+                "text-orange-300": message.isUserMessage,
+              })}
+            >
+              {format(new Date(message.createdAt), "HH:mm")}
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
